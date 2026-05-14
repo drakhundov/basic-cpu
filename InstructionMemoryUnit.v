15cc2e328465260d2bae6658d22462a915974849
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2026-05-14 19:56:40 +0300

    InstructionMemoryUnit Reads from ROM to InstructionRegister. Partial load implemented. ReadOnlyMemory

---
+++ InstructionMemoryUnit.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+
+module InstructionMemoryUnit (
+    input wire Clock,
+    input wire [15:0] Address,
+    input wire CS,
+    input wire LH,
+
+    output wire [15:0] IROut,
+    output wire [15:0] IMUOut // LSB of IROut.
+);
+  wire [7:0] MemOut;
+
+  ReadOnlyMemory IM (
+      .Address(Address),
+      .CS(CS),
+      .MemOut(MemOut)
+  );
+
+  InstructionRegister IR (
+      .I(MemOut),
+      .Clock(Clock),
+      .LH(LH),
+      .Write(CS),
+      .IROut(IROut)
+  );
+
+  // Represents address.
+  assign IMUOut = {8'b0, IROut[7:0]};
+endmodule
---
+++ ROM.v
@@ -0,0 +1,13 @@
+module ReadOnlyMemory (
+    input wire [15:0] Address,
+    input wire CS,
+    output reg [7:0] MemOut
+);
+
+  reg [7:0] ROM_DATA[0:65535];
+  initial $readmemh("ROM.mem", ROM_DATA);
+
+  always @(*) begin
+    MemOut = CS ? ROM_DATA[Address] : 8'hz;
+  end
+endmodule
